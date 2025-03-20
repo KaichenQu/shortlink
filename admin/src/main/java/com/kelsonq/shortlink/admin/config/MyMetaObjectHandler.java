7e18386489e0301cf103933e5b32d991f5b4b2bb
KaichenQu <qu.ka@northeastern.edu>
2025-03-20 00:24:55 -0700

    feature: Persistence layer auto fill

---
+++ admin/src/main/java/com/kelsonq/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package com.kelsonq.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import java.time.LocalDateTime;
+
+import java.util.Date;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+// java example
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+  @Override
+  public void insertFill(MetaObject metaObject) {
+    this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+    this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+    this.strictInsertFill(metaObject, "deleteFlag", () -> 0, Integer.class);
+  }
+
+  @Override
+  public void updateFill(MetaObject metaObject) {
+    this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+  }
+}
